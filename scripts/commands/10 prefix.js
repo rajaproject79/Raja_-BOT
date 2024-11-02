b40694fbe4b634cc4ecf44ef10daa43df33e324d
rajaproject79 <162680957+rajaproject79@users.noreply.github.com>
2024-11-02 08:32:09 +0600

    Update 10 prefix.js

--- scripts/commands/10 prefix.js
+++ scripts/commands/10 prefix.js
@@ -26,7 +26,7 @@ module.exports.handleEvent = async ({ api, event }) => {
     var time = moment.tz("Asia/Dhaka").format("LLLL");
   const text = `╭•┄┅═══❁🌺❁═══┅┄•╮\n${GP}\n╰•┄┅═══❁🌺❁═══┅┄•╯\n\n𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 : ${global.config.BOTNAME}\n𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 : ｢ ${global.config.PREFIX} ｣\n𝐑𝐎𝐁𝐎𝐓 𝐂𝐌𝐃: ｢ ${client.commands.size} ｣\n𝐓𝐈𝐌𝐄 : ${time}\n𝐆𝐑𝐎𝐔𝐏 𝐍𝐀𝐌𝐄: ${diptoName}\n`
   //const text2 = text[Math.floor(Math.random() * text.length)];
-const imgur = ["https://i.imgur.com/Yi0QXnn.jpeg"]
+const imgur = ["https://i.imgur.com/i39JjBs.jpeg",]
   const link = imgur[Math.floor(Math.random() * imgur.length)];
   const res = await dipto.get(link, { responseType: 'arraybuffer' })
 const ex = path.extname(link);
