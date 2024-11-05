3bb240cf66aca4fc341158dff5783ac4eccdecd0
rajaproject79 <162680957+rajaproject79@users.noreply.github.com>
2024-11-05 22:41:51 +0600

    Update Sex.js

--- scripts/commands/Sex.js
+++ scripts/commands/Sex.js
@@ -237,6 +237,6 @@ const fs = global.nodemodule["fs-extra"];
 
 "https://i.postimg.cc/fR6KgQHC/big-boobs-of-sexy-Pakistani-girl-exposed.jpg",
     ];
-   var callback = () => api.sendMessage({body:`লুচ্ছা বেডা😋+\nSố ảnh: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+   var callback = () => api.sendMessage({body:`RAJA 卝 চৌধুরী SEX PNG😋+\nSố ảnh: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
